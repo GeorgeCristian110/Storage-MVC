d347177a480d893473e61531637a2c26f6ecf13b
George Cristian Ciolponea <georgeciolponea@outlook.com>
2026-06-30 10:35:13 +0200

    Change a bit in product class

--- Storage/Models/Product.cs
+++ Storage/Models/Product.cs
@@ -4,11 +4,11 @@ namespace Storage.Models
 {
     public class Product
     {
-       public int Id { get; set; }
+        public int Id { get; set; }
         
-        [Required]
+        
         [StringLength(100)]
-        public string? Name { get; set; }
+        public required string Name { get; set; }
 
         [Range(0, 1000000)]
         public int Price { get; set; }
@@ -16,12 +16,12 @@ namespace Storage.Models
         [DataType(DataType.Date)]
         public DateTime OrderDate { get; set; }
         
-        [Required]
+        
         [StringLength(50)]
-        public string? Category { get; set; }
+        public required string Category { get; set; }
 
-        [Required,StringLength(10)]
-        public string? Shelf { get; set; }
+        [StringLength(10)]
+        public required string Shelf { get; set; }
 
         [Range(0, 10000)]
         public int Count { get; set; }
